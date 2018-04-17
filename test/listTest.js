9a6f2e4e14a6cfddecb55e03b32d73a6da696f11
Gavin Bauman <Gavin.Bauman@outlook.com>
2018-04-17 12:25:37 -0400

    Add test for listing extensions. Remove console log statement

---
+++ test/listTest.js
@@ -0,0 +1,34 @@
+const assert = require('assert')
+const factory = require('../').factory
+const fakeFs = require('memfs')
+
+/* eslint-env node, mocha */
+
+describe('azure-chaos', () => {
+  describe('ListTest', () => {
+    it('should properly list registered extensions', (done) => {
+      factory.ExtensionRegistry.configure({
+        fsLocation: '/temp',
+        fsImpl: fakeFs
+      })
+      const instance = factory.ExtensionRegistry.create()
+
+      instance.register({
+        extensionName: 'testExtension1',
+        extensionUri: 'testUri1',
+        extensionDesc: 'testDesc1'
+      }).then(() => {
+        instance.register({
+          extensionName: 'testExtension2',
+          extensionUri: 'testUri2',
+          extensionDesc: 'testDesc2'
+        }).then(() => {
+          return instance.getAll()
+        })
+          .then(array => {
+            assert.equal(array.length, 2)
+          }).then(done, done)
+      })
+    })
+  })
+})
