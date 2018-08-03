c54c8356b7dafadf8fa05eae4822eb0e0178c59b
Ben Greenier <ben@bengreenier.com>
2018-08-03 12:52:16 -0700

    Initial documentation and code (#1)
    * Initial documentation and code
    
    * add codeowners to make review requesting workflow easier
    
    * Add linter. Revised files accordingly
    
    * Add tests for extension registry, list, and request processor
    
    * Renamed chaos commands

---
+++ commands/unregister.js
@@ -0,0 +1,22 @@
+const factory = require('../lib/factory')
+
+exports.command = 'unregister <name>'
+
+exports.describe = 'unregister a chaos extension'
+
+exports.builder = {
+  name: {
+    description: 'the name of the extension to unregister'
+  }
+}
+
+exports.handler = (argv) => {
+  const registry = factory.ExtensionRegistry.create()
+  const logger = factory.Logger.create()
+
+  return registry
+    .unregister({
+      extensionName: argv.name
+    })
+    .then(logger.info.bind(logger), logger.error.bind(logger))
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  AzureAuthenticator: require('./lib/azure-authenticator'),
+  ExtensionRegistry: require('./lib/extension-registry'),
+  Extension: require('./lib/extension'),
+  Logger: require('./lib/logger'),
+  RequestProcessor: require('./lib/request-processor'),
+  factory: require('./lib/factory'),
+  register: require('./commands/register').handler,
+  list: require('./commands/list').handler,
+  unregister: require('./commands/unregister').handler,
+  start: require('./commands/start').handler,
+  stop: require('./commands/stop').handler,
+  resgen: require('./commands/resgen').handler,
+  token: require('./commands/token').handler
+}
