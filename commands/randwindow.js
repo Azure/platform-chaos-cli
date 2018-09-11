7e3758133d528a956053b447d799518f8bdceae3
Hannah Kennedy <hannah.kennedy@microsoft.com>
2018-09-11 11:35:56 -0400

    Adds a command for windowless random time to schedule an event.

--- commands/randwindow.js
+++ commands/randwindow.js
@@ -1,15 +1,46 @@
 const factory = require('../lib/factory')
 
-exports.command = 'randwindow <extension> <days> [days] <open> [hour] close [hour]'
+exports.command = 'randwindow <extension> [days] [open] [close]'
 
 exports.describe = 'schedule an upcoming time window to schedule some chaos'
 
 exports.builder = {
     extension: {
-        description: 'the name of the extension to start'
+        description: 'the name of the extension to start',
+        type: 'string',
+        required: false
     },
+    days: {
+        description: 'a formatted string with the selected days of the week, ie MWF',
+        type: 'string',
+        required: false
+    },
+    open: {
+        description: 'a formatted string of first moment when chaos window should start, ie 8:30',
+        type: 'string',
+        required: false
+    },
+    close: {
+        description: 'a formatted string of final moment when chaos window should be open, ie 16:00',
+        type: 'string',
+        required: false
+    }
+}
+
+exports.handler = (argv) => {
+    const logger = factory.Logger.create()
+
+    const parts = [
+        argv.extension,
+        argv.days,
+        argv.open,
+        argv.close
+    ]
+    // opentime = argv.open.split('h') // creates an array
+    // closetime = argv.close.split('h') // creates an array for close
 }
 
 //extension - kind of chaos to run?
-//days - M T W R F Sa Su
+//days - M T W R F Sa Su ; get today, then nearest selected days after
 //open - use 24 hour time, and then separate hour and minutes with an h?
+//close - ditto
